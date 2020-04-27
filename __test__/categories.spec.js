c4f6d1d5401d8cc255a9264f29c2fa259e04a08b
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2020-04-27 03:00:06 +0300

    Fix tests

--- __test__/categories.spec.js
+++ __test__/categories.spec.js
@@ -1,9 +1,11 @@
 import express            from 'express';
 import request            from 'supertest';
+
 import CategoryRepository from '../src/repositories/CategoryRepository.js';
 import UserRepository     from '../src/repositories/UserRepository.js';
 import CategoryRouter     from '../src/routers/CategoryRouter.js';
 import User               from '../src/models/User';
+
 
 const app = express();
 
@@ -20,19 +22,24 @@ jest.mock('../src/repositories/UserRepository.js');
 jest.mock('../src/repositories/CategoryRepository.js');
 
 describe('test categories route', () => {
+
     test('test categories GET method success answer', async () => {
-        const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZpQGlkaWNldmlzLmVkdSIsIm5hbWUiOiJIYXJyaWV0IFZhcmdhcyIsImlkIjoiMyIsImlhdCI6MTU4NzY0Njk1MiwiZXhwIjoxNTg3NzMzMzUyfQ.JhpIUjOp-J8cg08VRS_-WSa3_dGoOjIKQIY_DLr2Hlc';
+        const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IndpbHNvbkBmdXIuY29tIiwibmFtZSI6IldpbHNvbiIsImlkIjoiNyIsImlhdCI6MTU4NzcxNzM4MywiZXhwIjoxNTg3ODAzNzgzfQ.wAOCoPoYCnhBGygh8yyJp0jROsnOnK6huD6it9RNvEs';
 
         CategoryRepository.mockImplementation(() => ({
             getAllCategories: () => [1],
         }));
 
         const user = new User({
-            id: 3,
-            name: 'Harriet Vargas',
-            email: 'vi@idicevis.edu'
+            id: 7,
+            name: 'Wilson',
+            email: 'wilson@fur.com'
         });
-        user._password = '$2a$10$wOim5rfkQD2g.s3vq9JJEet6vTSbkq0X7c1FzFDMEipJShLXip2rG';
+        user._password = '$2a$10$Ou3rcS76Iv32lbxnQ61nu.7IM4Jx6liagoTP6mLle8upfb60wU6j6';
+
+        CategoryRepository.mockImplementation(() => ({
+            getAllCategories: () => [1],
+        }));
 
         UserRepository.mockImplementation(() => ({
             findByEmailAndId: () => user,
@@ -40,13 +47,12 @@ describe('test categories route', () => {
 
         const categoryRouter = new CategoryRouter(pool);
 
-        const res = await request(app.use('/api/categories', categoryRouter.router))
-            .get('/api/categories')
-            .set('Authorization', token);
+        const res = await request(app.use('/api/categories', categoryRouter.router)).get('/api/categories').set('Authorization', token);
 
         const response = res.body;
 
         expect(JSON.stringify(response))
             .toBe(JSON.stringify([1]));
     });
+
 });
